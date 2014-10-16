fb2b266ca8588f1a08fce5a016f04fedf509bd6d
gngeorgiev <gngeorgiev.it@gmail.com>
2014-10-16 23:14:27 +0300

    work on everlivefactory to create more effecient models to display

--- TelerikApp/js/controllers/usersController.js
+++ TelerikApp/js/controllers/usersController.js
@@ -1,4 +1,20 @@
 angular.module('controllers')
-    .controller('UsersCtrl', ['$scope', function () {
+    .controller('UsersCtrl', ['$scope', '$everlive',function ($scope, $everlive) {
+        $scope.users = [];
+        $scope.predicate = 'DisplayName';
+        $scope.refreshUsers = function () {
+            $everlive.Users.get()
+                .then(function (data) {
+                    $scope.users = data.result;
+                    $scope.$broadcast('scroll.refreshComplete');
 
+                    $everlive.Files.getDownloadUrlById($scope.users[0].Picture)
+                        .then(function(downloadUrl){
+                            document.getElementById('anchor1').href = downloadUrl;
+                        });
+
+                });
+        };
+
+        $scope.refreshUsers();
     }]);
--- TelerikApp/js/factories/everliveFactory.js
+++ TelerikApp/js/factories/everliveFactory.js
@@ -1,6 +1,45 @@
 angular.module('factories')
 
-    .factory('$everlive', ['EVERLIVE_API_KEY',
-        function (EVERLIVE_API_KEY) {
-            return new Everlive(EVERLIVE_API_KEY);
-        }]);
+    .factory('$everlive', ['EVERLIVE_API_KEY', '$q', function (EVERLIVE_API_KEY, $q) {
+        var service = new Everlive(EVERLIVE_API_KEY);
+        var defaultModelProperties = ['DisplayName', 'Email', 'Picture', 'Username'];
+
+        function getImageUrl(imageId) {
+            return service.Files.getDownloadUrlById(imageId)
+        }
+
+        function resizeImage(imageUrl) {
+
+        }
+
+        function buildModels(users, props) {
+            var finalUsers = [],
+                i = 0,
+                newUser,
+                currentProperty,
+                deferred = $q.defer();
+
+            props = props || defaultModelProperties;
+
+            users.forEach(function(user) {
+                newUser = {};
+                currentProperty = props[i];
+                for(;i < props.length; i++) {
+                    newUser[currentProperty] = user[currentProperty];
+                }
+
+                finalUsers.push(newUser);
+            });
+
+            return deferred.promise;
+        }
+
+        return {
+            service: service,
+            buildModel: buildModel,
+            getImageUrl: getImageUrl,
+            resizeImage: resizeImage
+        };
+
+
+    }]);
