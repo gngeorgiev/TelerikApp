9890f88dc7bebf9e63b342eea06bf41057387e2b
gngeorgiev <gngeorgiev.it@gmail.com>
2014-10-23 23:29:52 +0300

    update a unit test

--- test/specs/factoriesSpecs/everliveFactory.spec.js
+++ test/specs/factoriesSpecs/everliveFactory.spec.js
@@ -6,17 +6,7 @@ describe('Everlive Factory basic tests', function () {
         activitiesUrl,
         users;
 
-    users = [
-        {
-            DisplayName: 'User1'
-        },
-        {
-            DisplayName: 'User2'
-        },
-        {
-            DisplayName: 'User3'
-        }
-    ];
+    users = JSON.parse('[{"UserPicture":"https://bs2.cdn.telerik.com/v1/zOkB1dDKGOUzuPYN/44472990-52ff-11e4-9704-014be82b3616","Username":"andy","DisplayName":"Andy Gerald","Email":"andy.gerald@telerik.com","Gender":"1","BirthDate":"1990-12-02T22:00:00.000Z","IsVerified":false,"IdentityProvider":"Everlive","Role":"43775670-52ff-11e4-9704-014be82b3616","CreatedAt":"2014-10-13T17:35:06.445Z","ModifiedAt":"2014-10-13T17:35:06.445Z","CreatedBy":"00000000-0000-0000-0000-000000000000","ModifiedBy":"00000000-0000-0000-0000-000000000000","Owner":"44472995-52ff-11e4-9704-014be82b3616","Id":"44472995-52ff-11e4-9704-014be82b3616","Meta":{"Permissions":{"CanRead":true,"CanUpdate":false,"CanDelete":false}},"Liked":[]},{"UserPicture":"https://bs3.cdn.telerik.com/v1/zOkB1dDKGOUzuPYN/44470283-52ff-11e4-9704-014be82b3616","Username":"michael","DisplayName":"Michael Taylor","Email":"michael.taylor@telerik.com","Gender":"1","BirthDate":"1963-12-02T22:00:00.000Z","IsVerified":false,"IdentityProvider":"Everlive","Role":"43775670-52ff-11e4-9704-014be82b3616","CreatedAt":"2014-10-13T17:35:06.445Z","ModifiedAt":"2014-10-13T17:35:06.445Z","CreatedBy":"00000000-0000-0000-0000-000000000000","ModifiedBy":"00000000-0000-0000-0000-000000000000","Owner":"44472994-52ff-11e4-9704-014be82b3616","Id":"44472994-52ff-11e4-9704-014be82b3616","Meta":{"Permissions":{"CanRead":true,"CanUpdate":false,"CanDelete":false}},"Liked":[]},{"UserPicture":"https://bs3.cdn.telerik.com/v1/zOkB1dDKGOUzuPYN/44470282-52ff-11e4-9704-014be82b3616","BirthDate":"1982-12-02T22:00:00.000Z","CreatedAt":"2014-10-13T17:35:06.445Z","CreatedBy":"00000000-0000-0000-0000-000000000000","DisplayName":"Seth Peterson","Email":"seth.peterson@telerik.com","Gender":"1","IdentityProvider":"Everlive","IsVerified":false,"Liked":[{"Text":"It is finally time for graduation! Good job everyone, we made it.","CreatedBy":"44472994-52ff-11e4-9704-014be82b3616","Id":"44472996-52ff-11e4-9704-014be82b3616","User":[{"DisplayName":"Michael Taylor","Id":"44472994-52ff-11e4-9704-014be82b3616"}]}],"ModifiedAt":"2014-10-21T06:37:12.753Z","ModifiedBy":"00000000-0000-0000-0000-000000000000","Owner":"44472992-52ff-11e4-9704-014be82b3616","Role":"43775670-52ff-11e4-9704-014be82b3616","Username":"seth","Id":"44472992-52ff-11e4-9704-014be82b3616","Meta":{"Permissions":{"CanRead":true,"CanUpdate":false,"CanDelete":false}}}]');
 
     function fail(err) {
         throw new Error(err);
@@ -34,7 +24,7 @@ describe('Everlive Factory basic tests', function () {
             });
 
             if(!hasVal) {
-                fail(item + ' does not have a property with value from ' + values.join());
+                fail(JSON.stringify(item) + ' does not have a property with value from ' + values.join());
             }
         });
     }
@@ -58,13 +48,13 @@ describe('Everlive Factory basic tests', function () {
         activitiesUrl = url.replace('{{1}}', EVERLIVE_TYPES.Activities);
 
         $httpBackend.whenGET(/partials/).respond('');
+        $httpBackend.whenGET(usersUrl).respond({
+            Result: users
+        });
     }));
 
     describe('Users tests', function () {
         it('Should get 3 users with their displayNames', function (done) {
-            $httpBackend.whenGET(usersUrl).respond({
-                Result: users
-            });
 
             $everlive.getUsers().then(function (users) {
                 expect(users.length).toBe(3);
@@ -72,7 +62,6 @@ describe('Everlive Factory basic tests', function () {
                     expect(user.DisplayName).toBeDefined();
                 });
 
-                $httpBackend.resetExpectations();
                 done();
             });
 
@@ -80,15 +69,15 @@ describe('Everlive Factory basic tests', function () {
         });
 
         it('Should get 3 actual users', function (done) {
-            done();
-//            $httpBackend.whenGET(usersUrl).passThrough();
-//
-//            $everlive.getUsers().then(function (users) {
-//                expect(users.length).toBe(3);
-//                checkAllForProperty('username', users, ['michael', 'andy', 'seth']);
-//            });
-//
-//            $httpBackend.flush();
+
+            $everlive.getUsers().then(function (users) {
+                expect(users.length).toBe(3);
+                checkAllForProperty('Username', users, ['michael', 'andy', 'seth']);
+                expect(users[2].Liked.length).toBe(1);
+                done();
+            });
+
+            $httpBackend.flush();
         });
 
     });
